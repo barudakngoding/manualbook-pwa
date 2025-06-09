cf5ed7eea53450eacc5be57fb178399bc2d20d36
Erphan12 <muh.irfan161102@gmail.com>
2025-06-09 16:41:21 +0800

    initial commit - PWA Manualbook

---
+++ data_ekskavator.js
@@ -0,0 +1,114 @@
+const ekskavatorData = {
+    description: 'Manual book lengkap untuk Ekskavator KOMATSU 210 dengan berbagai komponen dan sistem operasional.',
+    subOptions: {
+        'General Komponen': {
+            description: 'Komponen utama ekskavator meliputi sistem mesin, pendinginan, dan struktur dasar yang menunjang kinerja optimal.',
+            images: [
+                {
+                    src: './images/KOMATSU 210/general komponen/bateray atau aki.jpeg',
+                    name: 'Baterai atau Aki'
+                },
+                {
+                    src: './images/KOMATSU 210/general komponen/engine.jpeg',
+                    name: 'Engine (Mesin)'
+                },
+                {
+                    src: './images/KOMATSU 210/general komponen/komponen luar.jpeg',
+                    name: 'Komponen Luar'
+                },
+                {
+                    src: './images/KOMATSU 210/general komponen/pump.jpeg',
+                    name: 'Pump (Pompa)'
+                },
+                {
+                    src: './images/KOMATSU 210/general komponen/radiator.jpeg',
+                    name: 'Radiator'
+                },
+                {
+                    src: './images/KOMATSU 210/general komponen/swing motor.jpeg',
+                    name: 'Swing Motor'
+                }
+            ]
+        },
+        'Lampu Panel': {
+            description: 'Panel kontrol dan indikator yang membantu operator dalam mengoperasikan ekskavator dengan aman dan efisien.',
+            images: [
+                {
+                    src: './images/KOMATSU 210/lampu panel/AC.jpeg',
+                    name: 'AC (Air Conditioner)'
+                },
+                {
+                    src: './images/KOMATSU 210/lampu panel/ALRM BUZZER.jpeg',
+                    name: 'Alarm Buzzer'
+                },
+                {
+                    src: './images/KOMATSU 210/lampu panel/AUTO DECELATOR.jpeg',
+                    name: 'Auto Decelator'
+                },
+                {
+                    src: './images/KOMATSU 210/lampu panel/AUTO.jpeg',
+                    name: 'Auto Mode'
+                },
+                {
+                    src: './images/KOMATSU 210/lampu panel/BUKU PADUAN.jpeg',
+                    name: 'Buku Panduan'
+                },
+                {
+                    src: './images/KOMATSU 210/lampu panel/HM DAN JAM.jpeg',
+                    name: 'Hour Meter dan Jam'
+                },
+                {
+                    src: './images/KOMATSU 210/lampu panel/MODE KERJA.jpeg',
+                    name: 'Mode Kerja'
+                },
+                {
+                    src: './images/KOMATSU 210/lampu panel/OFF.jpeg',
+                    name: 'Switch OFF'
+                },
+                {
+                    src: './images/KOMATSU 210/lampu panel/PENGATURAN.jpeg',
+                    name: 'Pengaturan'
+                },
+                {
+                    src: './images/KOMATSU 210/lampu panel/SWICTH BLOWER.jpeg',
+                    name: 'Switch Blower'
+                },
+                {
+                    src: './images/KOMATSU 210/lampu panel/SWITCH WIFER.jpeg',
+                    name: 'Switch Wiper'
+                },
+                {
+                    src: './images/KOMATSU 210/lampu panel/TEMPERATUR AC.jpeg',
+                    name: 'Temperatur AC'
+                },
+                {
+                    src: './images/KOMATSU 210/lampu panel/TEMPERATUR.jpeg',
+                    name: 'Temperatur Engine'
+                },
+                {
+                    src: './images/KOMATSU 210/lampu panel/TRAVEL SPEED.jpeg',
+                    name: 'Travel Speed'
+                },
+                {
+                    src: './images/KOMATSU 210/lampu panel/WATER WIFER.jpeg',
+                    name: 'Water Wiper'
+                }
+            ]
+        },
+        'Undercarriage': {
+            description: 'Sistem roda bawah yang terdiri dari track, sprocket, dan komponen pendukung untuk mobilitas ekskavator.',
+            images: [
+                {
+                    src: './images/KOMATSU 210/Undercarriage/komponen Undercarriage.jpeg',
+                    name: 'Komponen Undercarriage'
+                },
+                {
+                    src: './images/KOMATSU 210/Undercarriage/Undercarriage.jpeg',
+                    name: 'Undercarriage System'
+                }
+            ]
+        }
+    }
+};
+
+export default ekskavatorData;
